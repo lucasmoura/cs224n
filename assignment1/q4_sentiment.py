20876bd35ce46eb8f6943df63a44d1a3c3cf1e9d
Lucas Moura <lucas.moura128@gmail.com>
2017-08-14 15:12:27 -0300

    Implement sentence featurizer

--- assignment1/q4_sentiment.py
+++ assignment1/q4_sentiment.py
@@ -49,7 +49,8 @@ def getSentenceFeatures(tokens, wordVectors, sentence):
     sentVector = np.zeros((wordVectors.shape[1],))
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    sentVector = np.sum(wordVectors[tokens[word], :] for word in sentence)
+    sentVector /= len(sentence)
     ### END YOUR CODE
 
     assert sentVector.shape == (wordVectors.shape[1],)
